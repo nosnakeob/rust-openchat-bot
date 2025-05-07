07cd3c22cf9f870281387474d83f1099dcc84c4c
snake <827161257@qq.com>
2025-05-07 22:39:47 +0800

    refactor: 移除ctx_tokens字段

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,6 @@ pub struct TextGeneration<Wi: HubInfo> {
     tos: TokenOutputStream,
     logits_processor: LogitsProcessor,
     config: BaseConfig<Wi>,
-    ctx_tokens: Vec<u32>,
     eos_token_id: u32,
     ctx: ChatContext,
 }
@@ -56,34 +55,31 @@ impl<Wi: HubInfo> TextGeneration<Wi> {
             ),
             ctx: ChatContext::new(config.which.info().tokenizer_repo).await?,
             config,
-            ctx_tokens: Vec::with_capacity(1024),
             eos_token_id: eos_token,
         })
     }
 
     pub fn chat<'a>(&'a mut self, prompt: &'a str) -> impl Stream<Item = Result<String>> + 'a {
         let mut answer = String::with_capacity(1024);
-        let mut ans_tokens = vec![];
         self.ctx.push_msg(prompt);
 
         try_stream!({
             let prompt = self.ctx.render()?;
             // println!("prompt: {}", prompt);
-            self.ctx_tokens = self.str2tokens(&prompt)?;
+            let mut ctx_tokens = self.str2tokens(&prompt)?;
 
             let start = std::time::Instant::now();
 
-            let ans_start_idx = self.ctx_tokens.len();
+            let ans_start_idx = ctx_tokens.len();
 
             // 循环生成回答
             for index in 0..self.config.sample_len {
-                let next_token  = if index == 0 {
-                    self.gen_next_token(0, None)?
+                let next_token = if index == 0 {
+                    self.gen_next_token(&ctx_tokens, 0, None)?
                 } else {
-                    self.gen_next_token(ans_start_idx + index - 1, Some(ans_start_idx))?
+                    self.gen_next_token(&ctx_tokens, ans_start_idx + index - 1, Some(ans_start_idx))?
                 };
-                self.ctx_tokens.push(next_token);
-                ans_tokens.push(next_token);
+                ctx_tokens.push(next_token);
 
                 let decoded_token_opt = self.tos.next_token(next_token)?;
                 if let Some(t) = decoded_token_opt {
@@ -102,13 +98,12 @@ impl<Wi: HubInfo> TextGeneration<Wi> {
             }
 
             self.ctx.push_msg(&answer);
-            // println!("{:?}", ans_tokens);
             self.tos.clear();
 
             info!(
                 "speed: {:.2} token/s, total tokens: {}",
-                (self.ctx_tokens.len() - ans_start_idx) as f64 / start.elapsed().as_secs_f64(),
-                self.ctx_tokens.len()
+                (ctx_tokens.len() - ans_start_idx) as f64 / start.elapsed().as_secs_f64(),
+                ctx_tokens.len()
             );
         })
     }
@@ -124,11 +119,16 @@ impl<Wi: HubInfo> TextGeneration<Wi> {
         Ok(tokens)
     }
 
-    fn gen_next_token(&mut self, idx_pos: usize, ans_start_idx: Option<usize>) -> Result<u32> {
+    fn gen_next_token(
+        &mut self,
+        ctx_tokens: &Vec<u32>,
+        idx_pos: usize,
+        ans_start_idx: Option<usize>,
+    ) -> Result<u32> {
         let input = match ans_start_idx {
-            Some(_) => Tensor::new(&[*self.ctx_tokens.last().unwrap()], &self.config.device)?,
+            Some(_) => Tensor::new(&[*ctx_tokens.last().unwrap()], &self.config.device)?,
             // 首个字符
-            None => Tensor::new(&*self.ctx_tokens, &self.config.device)?,
+            None => Tensor::new(&**ctx_tokens, &self.config.device)?,
         }
         .unsqueeze(0)?;
 
@@ -138,7 +138,7 @@ impl<Wi: HubInfo> TextGeneration<Wi> {
         // 非首个字符应用惩罚
         if let Some(ans_start_idx) = ans_start_idx {
             if self.config.repeat_penalty != 1. {
-                let ans_tokens = &self.ctx_tokens[ans_start_idx..];
+                let ans_tokens = &ctx_tokens[ans_start_idx..];
                 let start_at = ans_tokens.len().saturating_sub(self.config.repeat_last_n);
                 logits = apply_repeat_penalty(
                     &logits,
